19cafe3c8140634db7a5e168e11924fb5dff935a
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2024-05-25 09:48:01 +0600

    updated deploy issues, - fixed serverless finction issues

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Application, Request, Response } from "express";
-import { bootstrapLoader } from "./server";
 import cors from "cors";
+import connectDB from "./config/DBConnection";
 
 const app: Application = express();
+
+connectDB();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/api", (req: Request, res: Response) => {
   res.send({ message: "Hello World!" });
+});
+
+app.get("*", (req: Request, res: Response) => {
+  res.status(404).send({ message: "Path not found!", option: "/api" });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,7 @@
 import app from "./app";
 import config from "./config";
-import connectDB from "./config/DBConnection";
 const port = config.port || 3000;
 
-export const bootstrapLoader = () => {
-  // CONNECTING TO DATABASE
-  connectDB();
-
-  try {
-    app?.listen(port, () => {
-      console.log(`E-commerce app listening on port ${port}`);
-    });
-  } catch (error) {
-    console.log(`server failed to start with error ${error}`);
-  }
-};
-
-bootstrapLoader(); //STARTING THE SERVER
+app.listen(port, () => {
+  console.log(`E-commerce app listening on port ${port}`);
+});
